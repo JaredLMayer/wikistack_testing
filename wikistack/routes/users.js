8ac374299a50d43c3c0d2531d7b67f3d160341ca
Eric P. Katz <ericpkatz@gmail.com>
2016-02-21 15:12:27 -0500

    run serially

--- wikistack/routes/users.js
+++ wikistack/routes/users.js
@@ -1,24 +1,31 @@
 var router = require('express').Router();
 var User = require('../db').models.User;
 var Page = require('../db').models.Page;
+var Promise = require('bluebird');
 
 router.get('/', function(req, res, next){
-  Page.aggregate({
-    $group: {
-      _id: '$author',
-      count: { $sum: 1 }
-    }
-  })
-  .exec(function(err, results){
-    var map = results.reduce(function(memo, item){
-      memo[item._id] = item.count;
-      return memo;
-    }, {});
-    User.find()
-      .then(function(users){
+
+  var aggregator = new Promise(function(resolve, reject){
+    Page.aggregate({
+      $group: {
+        _id: '$author',
+        count: { $sum: 1 }
+      }
+    }).exec(function(err, results){
+      if(err)
+        return reject(err);
+      resolve(results);
+    });
+  });
+
+  Promise.all([aggregator, User.find()])
+      .spread(function(agg, users){
+        var map = agg.reduce(function(memo, item){
+          memo[item._id] = item.count;
+          return memo;
+        }, {});
         res.render('users', { users: users, title: 'Users', map: map });
       });
-  });
 });
 
 
